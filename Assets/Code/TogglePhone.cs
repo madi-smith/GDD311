5d405e61d428389b0eaff66b9f9bd6d30062faa7
madi-smith <mesmith10@quinnipiac.edu>
2023-03-06 23:36:36 -0500

    phone ui

--- Assets/Code/TogglePhone.cs
+++ Assets/Code/TogglePhone.cs
@@ -15,4 +15,9 @@ public class TogglePhone : MonoBehaviour
     {
         GetComponent<RectTransform>().DOMove(new Vector3(5.16f, -1.8f, 0), 1f);
     }
+   /* public void TogglePhoneDown()
+    {
+        GetComponent<RectTransform>().DOMove(new Vector3(5f, -6.6f, 0), 1f);
+    }
+   */
 }
