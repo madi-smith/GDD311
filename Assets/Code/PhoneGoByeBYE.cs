ff1a32bc3d1e9edbf69a5fbb2e9b37e7681ebb25
KLeineAnton <90781064+KLeineAnton@users.noreply.github.com>
2023-04-18 02:45:27 -0400

    Phone GO Bye Bye

---
+++ Assets/Code/PhoneGoByeBYE.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneGoByeBYE : MonoBehaviour
+{
+    public void PhoneGone()
+    {
+        Destroy(this.gameObject);
+    }
+}
+
